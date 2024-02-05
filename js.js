a52ad5280fce8ac113ac55223dd644974d577a60
Mostafa Elsayed <moelsayed949@gmail.com>
2024-02-05 11:36:55 +0200

    add project

---
+++ js.js
@@ -0,0 +1,10 @@
+let body = document.querySelector(".body");
+let img = document.querySelector(".imgs");
+let btn = document.querySelector(".btn");
+
+function changeimag(x) {
+  img.src = x;
+}
+function changeback(y) {
+  body.style.background = y;
+}
